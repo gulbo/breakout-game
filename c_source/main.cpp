aa872c31eac41560592a137c82a1b499e787f6e8
hackasim <simonefinisimone@gmail.com>
2017-12-29 12:17:48 +0100

    Finished the function GameImplementation() using objects

--- c_source/main.cpp
+++ c_source/main.cpp
@@ -2,6 +2,7 @@
 #include "Board_Buttons.h"
 #include "Board_Led.h"
 #include <stdio.h>
+#include "game_elements.cpp"
 
 void await(unsigned int j){
 	int count;
@@ -9,60 +10,31 @@ void await(unsigned int j){
 }
 
 void GameInitialization(unsigned int difficulty){
-	//TODO IMPLEMENT WITH OBJECTS
+	
 	GLCD_Clear(Black);
 	int i=2;
 	int j=0;
 	short brick_color;
 	while(i*j<=1210){			//1452 to have 8 rows, 1210 to have 7 rows ecc...
-		switch(j){
-			case 0:
-				brick_color=Purple;
-				break;
-			case 1:
-				brick_color=Blue;
-				break;
-			case 2:
-				brick_color=Cyan;
-				break;
-			case 3:
-				brick_color=DarkGreen;
-				break;
-			case 4:
-				brick_color=Green;
-				break;
-			case 5:
-				brick_color=Yellow;
-				break;
-			case 6:
-				brick_color=Red;
-				break;
-		}
-		GLCD_DrawRect(270-10*j,i,8,20,brick_color);
+		
+		Brick b (i,270-10*j);
+		b.draw();
 		i+=24;
 		if(i==242){
 			i=2;
 			j++;
 		}
 	}
-	GLCD_DrawRect(270-10*j,218,8,20,brick_color);
+	Brick b(218,270-10*j);
+	b.draw();
 	
-	switch(difficulty){
-		case 0:
-			GLCD_DrawRect(5,90,10,60,White);
-			break;		
-		case 1:
-			GLCD_DrawRect(5,100,10,40,White);
-			break;
-		case 2:
-			GLCD_DrawRect(5,110,10,20,White);
-			break;
-	}
-	int k=100;
-	while(k>=15){
-		GLCD_DrawRect(k+1,117,6,6,Black);
-		GLCD_DrawRect(k,117,6,6,Magenta);
-		k--;
+	Paddle p(difficulty);
+	p.draw();
+	
+	Ball ball(117,100);
+	while(ball.y>=15){
+		ball.draw();
+		ball.y--;
 		delay(1);
 	}
 }
