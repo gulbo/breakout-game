2728ba91ab868b39a6a800ea231ab7aa1aaa39db
Andrea Gulberti <and.gulberti@gmail.com>
2017-12-29 15:21:39 +0100

    RTOS implemented, thread screen and thread game

--- RTE/_LPC1768BOARD/RTE_Components.h
+++ RTE/_LPC1768BOARD/RTE_Components.h
@@ -16,6 +16,8 @@
  */
 #define CMSIS_device_header "LPC17xx.h"
 
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
 #define RTE_DEVICE_STARTUP_LPC17XX      /* Device Startup for NXP17XX */
 #define RTE_Drivers_SPI0                /* Driver SPI0 */
         #define RTE_Drivers_SPI1                /* Driver SPI1 */
--- c_source/main.cpp
+++ c_source/main.cpp
@@ -3,12 +3,16 @@
 #include "Board_Led.h"
 #include <stdio.h>
 #include "game_elements.cpp"
+#include "cmsis_os.h"
 
-void await(unsigned int j){
-	int count;
-	for(count=0; count<j; count++);
-}
+#define SCREEN_DELAY 30 	//30ms -> 33Hz should be fine to avoid lag
+#define GAME_DELAY		5		//TODO find the correct value
 
+
+//RTOS threads
+osThreadId tid_screen;    //thread for the refresh of the screen
+osThreadId tid_game;			//thread for the game execution
+	
 void GameInitialization(unsigned int difficulty){
 	
 	GLCD_Clear(Black);
@@ -39,12 +43,53 @@ void GameInitialization(unsigned int difficulty){
 	}
 }
 
+void game(void const *argument){
+  for(;;){
+		//code.....
+		
+		osDelay(GAME_DELAY);
+  }
+}
+
+void refresh_screen(void const *argument){
+  for(;;){
+		//code.....
+		
+    osDelay(SCREEN_DELAY);
+  }
+}
+
+osThreadDef(game, osPriorityNormal, 1, 0);
+osThreadDef(refresh_screen, osPriorityNormal, 1, 0);
+
 int main(void){
-	
+  if (osKernelInitialize () != osOK){  //initialize the RTOS
+    //exit with an error message
+		return -1;
+  }
+ 
 	GLCD_Init();
 	LED_Initialize();
 	GameInitialization(1);
-	while(1){
+	
+	tid_game = osThreadCreate(osThread(game), NULL);
+	if (tid_game == NULL) {
+		//Failed to create the thread
+  }
+	tid_screen = osThreadCreate(osThread(refresh_screen), NULL);
+	if (tid_screen == NULL) {  
+		//Failed to create the thread
+  }
+
+	if (osKernelStart() != osOK){ //start the kernel
+		// kernel could not be started
+		return -1;
 	}
-	return 0;
+
+	osDelay(osWaitForever); //main thread waits forever
+	
+	//NO MAN'S LAND
+	while(1);
+	
+	return -1;
 }
